3dd3790d580b095217736c9c9a1d81a1aefaabf6
Brodie Blackburn <brodie.blackburn@gmail.com>
2022-03-27 09:43:23 +1000

    Hide token value

--- tests/test_up.py
+++ tests/test_up.py
@@ -5,13 +5,20 @@ import pytest
 from portfolio import up
 
 
+class SecretString(str):
+    """Obfuscates string in __repr__."""
+
+    def __repr__(self) -> str:
+        return repr("*****")
+
+
 @pytest.fixture
 def up_api_token():
     """Gets Up API token from environment variable."""
-    return os.environ["UP_API_TOKEN"]
+    return SecretString(os.environ["UP_API_TOKEN"])
 
 
-def test_ping():
+def test_ping(up_api_token):
     """Pings Up API."""
     assert up.ping(token=up_api_token).json()["meta"]["statusEmoji"] == "⚡️"
 
@@ -23,6 +30,6 @@ def test_api_authorization_error():
     assert response.json()["errors"][0]["status"] == "401"
 
 
-def test_get_balances():
+def test_get_balances(up_api_token):
     """Tests Balance directive is returned with expected currency code."""
     assert up.get_balances(token=up_api_token)[0].amount.currency == "AUD"

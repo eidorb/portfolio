ff3884003983e6c98f82ee6a4eec0ce64bcad11f
Brodie Blackburn <brodie.blackburn@gmail.com>
2022-06-18 14:56:36 +1000

    Bundle Lambda function dependencies
    - Downgrade Python to 3.9 to match Lambda runtime.
    - Bundle dependencies for x86_64 Lambda function architecture.
    - Create bundle-lambda-function Poetry script.
    - Upgrade Python and Node package dependencies.

--- portfolio/cdk/app.py
+++ portfolio/cdk/app.py
@@ -1,9 +1,16 @@
 """Portfolio AWS CDK app."""
 from pathlib import Path
+import shutil
+import subprocess
+
 
 import aws_cdk as cdk
 import aws_cdk.aws_lambda as lambda_
 import aws_cdk.aws_logs as logs
+
+
+# Path to directory containing Lambda function bundle.
+dist_path = Path(__file__).parent / "lambda-function/dist"
 
 
 class App(cdk.App):
@@ -35,6 +42,71 @@ class PortfolioStack(cdk.Stack):
                 auth_type=lambda_.FunctionUrlAuthType.NONE
             ).url,
         )
+
+
+def bundle_lambda_function():
+    """Bundles Lambda function dependencies for x86_64 Lambda function.
+
+    This bundles the correct dependencies regardless of whether we're running on
+    an M1 Mac or x86 Linux build pipeline.
+
+    Most of the dependencies are pure Python, except for PyYAML and MarkupSafe.
+
+    Additionally, the version of SQLite in the Lambda runtime is old. As a workaround,
+    we install pysqlite-binary. This package contain a more recent statically linked
+    version of SQLite.
+
+    So... to create a bundle of dependencies targeting the x86 Lambda function
+    architecture, we do the following steps:
+
+    - Build the Lambda function as a wheel.
+    - Install the wheel (and its dependencies) to a directory.
+    - Install PyYAML, MarkupSafe and pysqlite-binary wheels built for the
+      manylinux2014_x86_64, replacing any existing packages.
+
+    To be clear, unfortunately we cannot simply specify the manylinux2014_x86_64
+    platform when installing the wheel. The reason for this is that not all
+    dependencies offer wheels, and you cannot simultaneously specify a platform
+    and allow source builds.
+
+    Files are bundled in directory lambda-function/dist, relative to this module.
+    """
+    # Delete lambda-function/dist directory, relative to this module.
+    shutil.rmtree(dist_path, ignore_errors=True)
+
+    # Build the Lambda function as a Python wheel. We run the command from the Lambda
+    # function's directory in order for Poetry to see that directory's pyproject.toml
+    # file.
+    subprocess.run("poetry build --format wheel".split(), cwd=dist_path.parent)
+
+    # Install dependencies to lambda-function/dist/bundle directory.
+    subprocess.run(
+        [
+            "pip",
+            "install",
+            dist_path / "lambda_function-0.1.0-py3-none-any.whl",
+            "--target",
+            dist_path / "bundle",
+        ]
+    )
+
+    # Install PyYAML and MarkupSafe wheels, specifying the manylinux2014_x86_64
+    # platform.
+    subprocess.run(
+        [
+            "pip",
+            "install",
+            "--upgrade",
+            "PyYAML",
+            "MarkupSafe",
+            "pysqlite-binary",
+            "--target",
+            dist_path / "bundle",
+            "--platform",
+            "manylinux2014_x86_64",
+            "--only-binary=:all:",
+        ]
+    )
 
 
 if __name__ == "__main__":

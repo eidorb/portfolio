1a8ad6fe48065ff713d8a996bac59460c53c183a
Brodie Blackburn <brodie.blackburn@gmail.com>
2022-12-01 14:56:13 +1000

    Move all balance retrievals to loop

--- portfolio/balances.py
+++ portfolio/balances.py
@@ -16,11 +16,42 @@ def update(filename="balances.beancount") -> None:
     # and append to ledger file.
     for name, get_balances in (
         (
+            "Up",
+            lambda: up.get_balances(token=secrets.up.api_token),
+        ),
+        (
+            "Bitcoin",
+            lambda: [
+                bitcoin.get_balance(
+                    api_key=secrets.blockonomics.api_key,
+                    addresses=secrets.bitcoin.addresses,
+                    account=secrets.bitcoin.account,
+                )
+            ],
+        ),
+        (
+            "SelfWealth",
+            lambda: selfwealth.get_balances(
+                email=secrets.selfwealth.email,
+                password=secrets.selfwealth.password,
+                otp=pyotp.TOTP(secrets.selfwealth.totp_key).now(),
+            ),
+        ),
+        (
             "State Custodians",
             lambda: statecustodians.get_balances(
                 user_id=secrets.statecustodians.user_id,
                 password=secrets.statecustodians.password,
             ),
+        ),
+        (
+            "Bankwest",
+            lambda: [
+                bankwest.get_balance(
+                    pan=secrets.bankwest.pan,
+                    password=secrets.bankwest.password,
+                )
+            ],
         ),
     ):
         # Open ledger file in append mode.
@@ -32,41 +63,3 @@ def update(filename="balances.beancount") -> None:
                 logger.info("Wrote %s balances to %s.", name, file.name)
             except Exception:
                 logger.error("Failed to get %s balances.", name, exc_info=True)
-
-    # Open ledger file in append mode.
-    with open(filename, mode="a") as file:
-        up_balances = up.get_balances(token=secrets.up.api_token)
-        logger.info("Retrieved Up account balances.")
-        printer.print_entries(up_balances, file=file)
-        logger.info("Wrote Up account balances to %s.", file.name)
-
-        bitcoin_balance = bitcoin.get_balance(
-            api_key=secrets.blockonomics.api_key,
-            addresses=secrets.bitcoin.addresses,
-            account=secrets.bitcoin.account,
-        )
-        logger.info("Retrieved Bitcoin balance.")
-        printer.print_entries([bitcoin_balance], file=file)
-        logger.info("Wrote Bitcoin balance to %s.", file.name)
-
-        try:
-            selfwealth_balances = selfwealth.get_balances(
-                email=secrets.selfwealth.email,
-                password=secrets.selfwealth.password,
-                otp=pyotp.TOTP(secrets.selfwealth.totp_key).now(),
-            )
-            logger.info("Retrieved SelfWealth holdings balances.")
-            printer.print_entries(selfwealth_balances, file=file)
-            logger.info("Wrote SelfWealth holdings balances to %s.", file.name)
-        # .get_balances() started raising this exception, but only from within GitHub
-        # Actions workflows. Catch it so that other balances can still be retrieved.
-        except TypeError:
-            logger.error("Failed to get SelfWealth holdings balances.", exc_info=True)
-
-        bankwest_balance = bankwest.get_balance(
-            pan=secrets.bankwest.pan,
-            password=secrets.bankwest.password,
-        )
-        logger.info("Retrieved Bankwest account balance.")
-        printer.print_entries([bankwest_balance], file=file)
-        logger.info("Wrote Bankwest account balance to %s.", file.name)

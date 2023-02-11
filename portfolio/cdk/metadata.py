bf0874dfc73023f72fce76755b891bdee8e57603
Brodie Blackburn <brodie.blackburn@gmail.com>
2023-02-11 14:01:53 +1000

    Add change dashboard

--- portfolio/cdk/metadata.py
+++ portfolio/cdk/metadata.py
@@ -12,5 +12,71 @@ metadata = {
         "datasette-redirect-forbidden": {
             "redirect_to": "/-/github-auth-start",
         },
+        "datasette-dashboards": {
+            "dashboard": {
+                "title": "Portfolio allocations",
+                "charts": {
+                    "change": {
+                        "title": "Change required to reach target allocations",
+                        "db": "portfolio",
+                        "query": "select * from changes order by change desc",
+                        "library": "vega",
+                        "display": {
+                            "encoding": {
+                                "x": {
+                                    "field": "change",
+                                    "type": "quantitative",
+                                    "stack": None,
+                                    "axis": None,
+                                },
+                                "y": {
+                                    "field": "asset_class",
+                                    "type": "nominal",
+                                    "sort": None,
+                                    "axis": None,
+                                },
+                                "color": {
+                                    "value": "red",
+                                    "condition": {
+                                        "test": "datum.change>0",
+                                        "value": "limegreen",
+                                    },
+                                },
+                            },
+                            "layer": [
+                                {"mark": "bar"},
+                                {
+                                    "mark": {
+                                        "type": "text",
+                                        "align": {
+                                            "expr": "datum.change>0?'right':'left'"
+                                        },
+                                        "dx": {"expr": "datum.change>0?-4:4"},
+                                    },
+                                    "encoding": {
+                                        "text": {"field": "asset_class"},
+                                        "color": {"value": "black"},
+                                        "x": {"datum": 0},
+                                    },
+                                },
+                                {
+                                    "mark": {
+                                        "type": "text",
+                                        "align": {
+                                            "expr": "datum.change>0?'left':'right'"
+                                        },
+                                        "dx": {"expr": "datum.change>0?4:-4"},
+                                    },
+                                    "encoding": {
+                                        "text": {"field": "change", "format": ".1%"},
+                                        "color": {"value": "black"},
+                                    },
+                                },
+                            ],
+                        },
+                    }
+                },
+            }
+        },
     },
 }

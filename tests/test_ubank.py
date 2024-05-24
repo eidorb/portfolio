2b2f669b4f55e6edb6cd39ff1065e448ffc1bdcb
Brodie Blackburn <brodie.blackburn@gmail.com>
2024-05-24 10:38:19 +1000

    Rework ubank balances to work with updated package
    - ubank device credentials are stored in AWS Parameter Store
    - Stored device credentials are rotated
    - Balances read from slightly different API endpoint
    - Drop the cookie-changing AST nonsense
    - Balance update code is not special

--- tests/test_ubank.py
+++ tests/test_ubank.py
@@ -1,69 +1,24 @@
-import ast
-import importlib.resources
-from tempfile import NamedTemporaryFile
+import ubank
 
-import pytest
+from portfolio.ubank import get_balances, get_device, save_device
 
-from portfolio import secrets, ubank
-from portfolio.ubank import CookieTransformer, change_cookie
-from tests import SecretDict, SecretString
+# Patch Device.__repr__ to hide secrets.
+ubank.Device.__repr__ = lambda self: "Device(hardware_id='***', ...)"
 
 
-@pytest.fixture
-def username():
-    return SecretString(secrets.ubank.username)
-
-
-@pytest.fixture
-def password():
-    return SecretString(secrets.ubank.password)
-
-
-@pytest.fixture
-def cookie():
-    return SecretDict(secrets.ubank.cookie)
-
-
-def test_get_balances(username, password, cookie):
+def test_get_balances():
     """Tests Balance directive is returned with expected accounts."""
-    balances, trusted_cookie = ubank.get_balances_and_cookie(username, password, cookie)
+    balances = get_balances(get_device())
     accounts = [balance.account.split(":")[-1] for balance in balances]
     assert "USave" in accounts
     assert "USpend" in accounts
-    assert "name" in cookie
 
 
-def test_cookie_transformer():
-    module = ast.parse(
-        """
-class Class:
-    cookie = {'a': 1, 'b': 2}"""
-    )
-    assert module.body[0].body[0].value.values[0].value == 1
-    assert module.body[0].body[0].value.values[1].value == 2
-
-    cookie = {"c": 3, "d": 4}
-    transformed = CookieTransformer(cookie).visit(module)
-
-    # The dict values should be transformed.
-    assert transformed.body[0].body[0].value.values[0].value == 3
-    assert transformed.body[0].body[0].value.values[1].value == 4
-
-    # The dict should be replaced with cookie.
-    assert ast.dump(transformed.body[0].body[0].value) == ast.dump(
-        ast.parse(str(cookie), mode="eval").body
-    )
+def test_get_device():
+    """Tests restoring enrolled ubank device from Parameter Store."""
+    assert get_device().device_meta[0] == "{"
 
 
-def test_change_cookie():
-    """Tests cookie dict is replaced in module file."""
-    with NamedTemporaryFile("w") as module_file:
-        traversable = importlib.resources.files().joinpath(module_file.name)
-        module_file.write(
-            """
-class Class:
-    cookie = {"a": 1, "b": 2}"""
-        )
-        module_file.flush()
-        change_cookie({"replacement": "cookie"}, traversable)
-        assert traversable.read_text().endswith("{'replacement': 'cookie'}")
+def test_save_device():
+    """Tests saving enrolled ubank device to Parameter Store."""
+    assert save_device(get_device())["Version"]
